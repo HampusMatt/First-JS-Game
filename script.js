f7cf24ef2391ba5cb5436a872d279a0fc42a0808
HampusMatt <hampusm.mattson@gmail.com>
2022-12-21 18:32:50 +0000

    Add some JS

--- script.js
+++ script.js
@@ -10,6 +10,10 @@ const questions = [
 
 const startButton = document.getElementById('start-btn');
 const questionContainerElement = document.getElementById('question-container');
+const questionElement = document.getElementById('question');
+const answerButtonsElement = document.getElementById('answer-buttons');
+
+let shuffledQuestions, currentQuestionIndex;
 
 startButton.addEventListener('click', startGame);
 
@@ -17,10 +21,18 @@ startButton.addEventListener('click', startGame);
 function startGame () {
     
     startButton.classList.add('hide');
+    shuffledQuestions = questions.sort(() => Math.random() - .5);
+    currentQuestionIndex = 0;
     questionContainerElement.classList.remove('hide');
     setNextQuestion ();
 }
 
-function setNextQuestion () {}
+function setNextQuestion () {
+    showQuestion(shuffledQuestions[currentQuestionIndex]);
+}
+
+function showQuestion(question) {
+    questionElement.innerText = question.question;
+}
 
 function selectAnswer () {}
