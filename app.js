717ebc8c35cb6948aecbcc1b6a63a44ce94e7888
HampusMatt <hampusm.mattson@gmail.com>
2022-12-25 16:32:03 +0000

    Add more JS code to btns

--- app.js
+++ app.js
@@ -74,9 +74,30 @@ const menu = [
 ];
 
 const sectionCenter = document.querySelector(".section-center");
+const filterBtns = document.querySelectorAll(".filter-btn");
 
 window.addEventListener("DOMContentLoaded", function() {
-  let displayMenu = menu.map(function(item) {
+ displayMenuItems(menu);
+});
+
+filterBtns.forEach(function(btn){
+  btn.addEventListener("click", function(event) {
+    let category = event.currentTarget.dataset.id;
+    let menuCategory = menu.filter(function(menuItem){
+      if (menuItem.category === category) {
+        return menuItem;
+      };
+    });
+    if (category === "all"){
+      displayMenuItems(menu);
+    } else {
+      displayMenuItems(menuCategory);
+    };
+  });
+});
+
+function displayMenuItems(menuItems) {
+  let displayMenu = menuItems.map(function(item) {
 
     return `<article class="menu-item">
     <img src=${item.img} class="photo" alt=${item.title}>
@@ -91,4 +112,4 @@ window.addEventListener("DOMContentLoaded", function() {
   });
   displayMenu = displayMenu.join("")
   sectionCenter.innerHTML = displayMenu;
-});
+}
