ef431c597a7fdf47902a4c65e8dd8cf29408d471
HampusMatt <hampusm.mattson@gmail.com>
2022-12-26 14:51:02 +0000

    Add dynamic JS code to project

--- app.js
+++ app.js
@@ -71,16 +71,36 @@ const menu = [
     img: "./images/item-9.jpeg",
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
+  {
+    id: 10,
+    title: "steak dinner",
+    category: "dinner",
+    price: 39.99,
+    img: "./images/item-10.jpeg",
+    desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+  },
 ];
 
 const sectionCenter = document.querySelector(".section-center");
-const filterBtns = document.querySelectorAll(".filter-btn");
+const container = document.querySelector(".btn-container")
+
 
 window.addEventListener("DOMContentLoaded", function() {
  displayMenuItems(menu);
-});
+ const categories = menu.reduce(function(values, item) {
+  if(!values.includes(item.category)){
+    values.push(item.category);
+  }
+  return values;
+ },["all"]);
 
-filterBtns.forEach(function(btn){
+ const categoryBtns = categories.map(function(category) {
+  return `<button class="filter-btn" type="button" data-id=${category}>${category}</button>`
+ }).join("");
+
+ container.innerHTML = categoryBtns;
+ const filterBtns = document.querySelectorAll(".filter-btn");
+ filterBtns.forEach(function(btn){
   btn.addEventListener("click", function(event) {
     let category = event.currentTarget.dataset.id;
     let menuCategory = menu.filter(function(menuItem){
@@ -95,6 +115,9 @@ filterBtns.forEach(function(btn){
     };
   });
 });
+});
+
+
 
 function displayMenuItems(menuItems) {
   let displayMenu = menuItems.map(function(item) {
