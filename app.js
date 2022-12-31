d04c5d1aa4b5e46f2627d9a41269204e72bb69e9
HampusMatt <hampusm.mattson@gmail.com>
2022-12-31 15:25:49 +0000

    add JS code to project

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
-// Definiera variabler
 const btns = document.querySelectorAll(".tab-btn");
-const about = document.querySelector(".about")
-const articles = document.querySelectorAll(".content")
+const articles = document.querySelectorAll(".content");
+const about = document.querySelector(".about");
 
-
-//Missade = hur vet jag vad som blir klickad på??
-
-// Sätta ett onclick-event som låter mig byta active class från en btn + content
-// till den som har blivit vald.
-
-about.addEventListener("click", function(e) {
-    const id = e.target.dataset.id;
+about.addEventListener("click", function (event) {
+    const id = event.target.dataset.id;
 
     if (id) {
         btns.forEach(function (btn) {
             btn.classList.remove("active");
-            e.target.classList.add("active");
+            event.target.classList.add("active");
         });
         articles.forEach(function (article) {
             article.classList.remove("active");
         });
-        const element = document.getElementById(id);
-        element.classList.add("active"); 
+        let showPara = document.getElementById(id);
+        showPara.classList.add("active");
     }
-});
+})
+
+
+// 1 = hämta variabler 1P
+// 2 = lägg till ett onclick event som kollar vilken btn som tryckts på. 1P
+// 3 = Om det finns ett id = ta bort active på alla knappar och lägg till active klass på btn. 1P
+// 4 = ta bort active från alla artiklar. 1P
+// 5 = Lägg till active klass på den paragraf som matchar id med btn som klickats. 1P
