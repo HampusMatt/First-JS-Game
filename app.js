5ac3e203e80e206ca8d5c34ea950926b75b625b3
HampusMatt <hampusm.mattson@gmail.com>
2023-01-01 12:50:53 +0000

    Add new code

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+// set inital value to zero
+let count = 0;
+// select value and buttons
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
 
-const btn = document.getElementById("btn");
-const color = document.querySelector(".color");
+btns.forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
+    const styles = e.currentTarget.classList;
+    if (styles.contains("decrease")) {
+      count--;
+    } else if (styles.contains("increase")) {
+      count++;
+    } else {
+      count = 0;
+    }
 
-btn.addEventListener("click", function () {
-  const randomNumber = 2;
-  document.body.style.backgroundColor = colors[randomNumber];
-  color.textContent = colors[randomNumber];
-})
+    if (count > 0) {
+      value.style.color = "green";
+    }
+    if (count < 0) {
+      value.style.color = "red";
+    }
+    if (count === 0) {
+      value.style.color = "#222";
+    }
+    value.textContent = count;
+  });
+});
